7d82999628b921fa557ab7a0063e8a9c003d1828
Aleegalan06 <alejandrogalanlavellaneda@gmail.com>
2026-06-02 22:40:13 +0200

    BaseApp

---
+++ entrenamiento.js
@@ -0,0 +1,22 @@
+const nombreEjercicio = document.getElementById("nombreEjercicio")
+const pesoEjercicio = document.getElementById("pesoEjercicio")
+const numeroSeries = document.getElementById("numeroSeries")
+const numeroRepes = document.getElementById("numeroRepes")
+const parrafoEjercicios = document.getElementById("parrafoEjercicios")
+let contadorEjercicios = 0
+const añadirEj = document.getElementById("añadirEj")
+const terminarEntrenamiento = document.getElementById("terminarEntrenamiento")
+añadirEj.addEventListener("click", function() {
+    contadorEjercicios++
+    nombreEjercicio.value = ""
+    pesoEjercicio.value = ""
+    numeroSeries.value = ""
+    numeroRepes.value = ""
+    parrafoEjercicios.innerHTML = "Ejercicios: " + contadorEjercicios + 
+    ' <input type="button" value="Añadir" id="añadirEj"><br>' +
+    ' <input type="button" value="Terminar entrenamiento" id="terminarEntrenamiento">'
+})
+terminarEntrenamiento.addEventListener("click", function(){
+    alert("Entrenamiento Terminado")
+    window.location.href = "index.html"
+})
---
+++ index.js
@@ -0,0 +1,11 @@
+const botonEmpezar = document.getElementById("empezarEntrenamiento")
+const nombreUsuario = document.getElementById("nombreUsuario")
+const fecha = document.getElementById("fecha")
+botonEmpezar.addEventListener("click", function(){
+    //Recoge el valor del nombredeusuario que los pasa a minusculaTodos
+    const valorNombre = nombreUsuario.value.toLowerCase()
+    //Recoge el valor de la fecha de hoy
+    const valorFecha = fecha.value
+    //Y redirige a entrenamiento.html
+    window.location.href = "entrenamiento.html" 
+})
